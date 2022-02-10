16b22c70976d52e2691a13d6213303ad50bd556e
이경은 <2kyung19@gmail.com>
2022-02-10 12:59:25 +0900

    feat : router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import SignIn from 'pages/SignIn';
+import NotFound from 'pages/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function App(): JSX.Element {
-  return <div className="App">hello</div>;
+function Home(): JSX.Element {
+  return <div>hello</div>;
 }
 
-export default App;
+export default Home;
